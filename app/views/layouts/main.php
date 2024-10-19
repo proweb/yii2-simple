054d13d92a18f13f802689d7c2cbd71c1d9a0b7e
Sergey Mochalov <proweb@users.noreply.github.com>
2024-10-19 23:20:16 +0300

    Change web to public_html name

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -69,7 +69,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 </main>
 
 <footer id="footer" class="mt-auto py-3 bg-light">
-    <div class="container">
+    <div class="container"> 
         <div class="row text-muted">
             <div class="col-md-6 text-center text-md-start">&copy; My Company <?= date('Y') ?></div>
             <div class="col-md-6 text-center text-md-end"><?= Yii::powered() ?></div>
