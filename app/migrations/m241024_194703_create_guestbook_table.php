40723c88a76ba55b7c00ee63e8f4764641b1a190
Sergey Mochalov <proweb@users.noreply.github.com>
2024-10-30 00:29:38 +0300

    Migration for guests messages

---
+++ app/migrations/m241024_194703_create_guestbook_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+use yii\db\Schema;
+
+/**
+ * Handles the creation of table `{{%guestbook}}`.
+ */
+class m241024_194703_create_guestbook_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%guestbook}}', [
+            'id' => Schema::TYPE_PK,
+            'name' => Schema::TYPE_STRING,
+            'email' => Schema::TYPE_STRING . ' NOT NULL',            
+            'message' => Schema::TYPE_TEXT . ' NOT NULL',
+            'created_at' => Schema::TYPE_INTEGER . ' NOT NULL'
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%guestbook}}');
+    }
+}
